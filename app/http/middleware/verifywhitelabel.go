9269783a214d20c49086e57ac7a9793b894ee271
Noah <noahgonzales281@gmail.com>
2025-02-23 16:13:11 -0700

    verify always

--- app/http/middleware/verifywhitelabel.go
+++ app/http/middleware/verifywhitelabel.go
@@ -1,45 +1,12 @@
 package middleware
 
 import (
-	"fmt"
-	"github.com/TicketsBot/GoPanel/config"
-	"github.com/TicketsBot/GoPanel/rpc"
-	"github.com/TicketsBot/GoPanel/utils"
-	"github.com/TicketsBot/common/premium"
 	"github.com/gin-gonic/gin"
 )
 
 func VerifyWhitelabel(isApi bool) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
-		userId := ctx.Keys["userid"].(uint64)
-
-		tier, err := rpc.PremiumClient.GetTierByUser(ctx, userId, false)
-		if err != nil {
-			ctx.JSON(500, utils.ErrorJson(err))
-			return
-		}
-
-		if tier < premium.Whitelabel {
-			var isForced bool
-			for _, id := range config.Conf.ForceWhitelabel {
-				if id == userId {
-					isForced = true
-					break
-				}
-			}
-
-			if !isForced {
-				if isApi {
-					ctx.AbortWithStatusJSON(402, gin.H{
-						"success": false,
-						"error":   "You must have the whitelabel premium tier",
-					})
-				} else {
-					ctx.Redirect(302, fmt.Sprintf("%s/premium", config.Conf.Server.MainSite))
-					ctx.Abort()
-				}
-				return
-			}
-		}
+		// Always allow requests to proceed
+		ctx.Next()
 	}
 }
